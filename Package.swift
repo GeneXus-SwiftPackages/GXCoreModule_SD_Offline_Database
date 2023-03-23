8992ee11bfd1532d6d86a906194615d8cdd89693
genexusbot <builder@genexus.com>
2023-03-23 10:28:24 -0300

    Package version: 1.0.0-beta+20230323132058

--- Package.swift
+++ Package.swift
@@ -23,8 +23,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreModule_SD_Offline_Database",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_Offline_Database-1.0.0-beta+20230323104412.xcframework.zip",
-			checksum: "829687453739179179f3f7c60827e7d9079f29bc4ab471c7a7e57d69568159f2"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXCoreModule_SD_Offline_Database-1.0.0-beta+20230323132058.xcframework.zip",
+			checksum: "853ee9c6b854a9e5a2c4fad945df877056007164c84eee96f604ba8e398e5be5"
 		)
 	]
 )
