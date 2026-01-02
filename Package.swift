3dbafd5b1ddfcb47fb7b9a2a7681c63f596a1661
The iOS Team @ GeneXus <builder@genexus.com>
2026-01-02 10:52:22 -0500

    Package version: 4.0.6

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
 			targets: ["GXCoreModule_SD_Offline_DatabaseWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreBL.git", exact: "4.0.5"),
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXDataLayerLocal.git", exact: "4.0.5")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreBL.git", exact: "4.0.6"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXDataLayerLocal.git", exact: "4.0.6")
 	],
 	targets: [
 		.target(name: "GXCoreModule_SD_Offline_DatabaseWrapper",
@@ -23,8 +23,8 @@ let package = Package(
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCoreModule_SD_Offline_Database",
-			url: "https://pkgs.genexus.dev/iOS/releases/GXCoreModule_SD_Offline_Database-4.0.5.xcframework.zip",
-			checksum: "61eb4f6c6a637c0d69fbb5b2e91afede832c6e38d619cfb726733a5df61c0f82"
+			url: "https://pkgs.genexus.dev/iOS/releases/GXCoreModule_SD_Offline_Database-4.0.6.xcframework.zip",
+			checksum: "43bfa2ef2487937c064e377f0f667a6fa13fb6b461b578253e79440ded6183ba"
 		)
 	]
 )
